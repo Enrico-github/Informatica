88405f7bad805384cf971364760311eb9d813ecf
Enrico Prearo <121225398+Enrico-github@users.noreply.github.com>
2025-10-25 11:05:54 +0200

    Delete Esercitazioni/EsX1810 directory

--- Esercitazioni/EsX1810/Es1_SQL/create_table.sql
+++
@@ -1,7 +0,0 @@
-create table autori(
-codice int primary key,
-cf int,
-cognome varchar(20),
-nome varchar(20),
-citta varchar(20)
-);
--- Esercitazioni/EsX1810/Es1_SQL/modify_table.sql
+++
@@ -1,6 +0,0 @@
-alter table autori
-change cf codice_fiscale char(16),
-add indirizzo varchar(40),
-add CAP char(5),
-modify cognome varchar(40) not null,
-modify nome varchar(40) not null;
