e331a96c01fb45d96028cd10ddf713ca59aa15c1
Enrico Prearo <121225398+Enrico-github@users.noreply.github.com>
2026-01-24 11:04:16 +0100

    Create corsi.php

---
+++ PHP/Es2/corsi.php
@@ -0,0 +1,28 @@
+<?php
+$corsi = [
+    "Sistemi e reti",
+    "Robotica",
+    "Contabilità", "Meccatronica",
+    "Chimica",
+    "Statistica",
+    "Matematica",
+    "Informatica",
+    "Marketing",
+    "Economia Politica"
+]
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<h2>ECCO I CORSI A DSIPOSIZIONE</h2>
+<script src="script.js"></script>
+</body>
+</html>
