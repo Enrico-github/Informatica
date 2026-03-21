7b0248bdb78d5c67b5e2862ac1f430730d760613
Enrico Prearo <121225398+Enrico-github@users.noreply.github.com>
2026-03-21 11:25:53 +0100

    Create index.php

---
+++ PHP/COOKIE/index.php
@@ -0,0 +1,21 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<a href="show.php">https/show.php</a>
+<form method = "POST" action = "action.php">
+      <label for="nome" >nome:</label>
+        <input id="nome" type="text" name="nome">
+    <br>
+        <button type="submit">INVIA</button>
+</form>
+</body>
+</html>
